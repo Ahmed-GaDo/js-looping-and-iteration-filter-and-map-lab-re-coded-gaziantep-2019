d7bea7367804448411a11fcd76671351b76dea85
Ahmed Gado <a.gado.2df@gmail.com>
2019-08-02 17:47:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution here:
+<<<<<<< HEAD
+=======
+// Code your solution here:
+>>>>>>> 98b3b1398864de47e7f968525527f2ea5350685b
 function driversWithRevenueOver (drivers, revenueValue){
     const newDriversFilterList = drivers.filter(
       function (driver) { return driver.revenue > revenueValue; }
